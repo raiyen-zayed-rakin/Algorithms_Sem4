6e902bf65fce413655e95995bb05521065f21833
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-05-02 16:26:39 +0600

    new

--- Lab Final/1no.cpp
+++ Lab Final/1no.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iomanip> // for std::setw
+#include <iomanip>
 
 using namespace std;
 
-int knapsack(int W, vector<int>& wt, vector<int>& val, int n, vector<vector<int>>& dp) {
+int knapsack(int W, vector<int>& wt, vector<int>& val, int n, vector<vector<int>>& dp, vector<bool>& included) {
     for (int i = 1; i <= n; ++i) {
         for (int w = 1; w <= W; ++w) {
             if (wt[i - 1] <= w) {
@@ -15,6 +15,16 @@ int knapsack(int W, vector<int>& wt, vector<int>& val, int n, vector<vector<int>
             }
         }
     }
+
+    // Backtracking
+    int remainingWeight = W;
+    for (int i = n; i > 0 && remainingWeight > 0; --i) {
+        if (dp[i][remainingWeight] != dp[i - 1][remainingWeight]) {
+            included[i - 1] = true;
+            remainingWeight -= wt[i - 1];
+        }
+    }
+
     return dp[n][W];
 }
 
@@ -26,8 +36,10 @@ int main() {
     vector<int> profit(n), weight(n);
     cout << "Enter the profits and weights of " << n << " items:\n";
     for (int i = 0; i < n; i++) {
-        cout << "Profit and weight of item " << i + 1 << ": ";
-        cin >> profit[i] >> weight[i];
+        cout << "Profit of item " << i + 1 << ": ";
+        cin >> profit[i];
+        cout << "weight of item " << i + 1 << ": ";
+        cin >> weight[i];
     }
 
     int W;
@@ -35,19 +47,25 @@ int main() {
     cin >> W;
 
     vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+    vector<bool> included(n, false);
 
-    int max_profit = knapsack(W, weight, profit, n, dp);
+    int max_profit = knapsack(W, weight, profit, n, dp, included);
 
-    // Display the maximum profit
     cout << "Maximum profit: " << max_profit << endl;
 
-    // Display the DP table
     cout << "\nDP Table:" << endl;
     for (int i = 0; i <= n; ++i) {
         for (int j = 0; j <= W; ++j) {
-            cout << setw(2) << setfill('0') << dp[i][j] << " ";
+            cout << setw(3) << dp[i][j] << " ";
         }
         cout << endl;
+    }
+
+    cout << "\nItems included in the knapsack:" << endl;
+    for (int i = 0; i < n; ++i) {
+        if (included[i]) {
+            cout << "Item " << i + 1 << ": Profit = " << profit[i] << ", Weight = " << weight[i] << endl;
+        }
     }
 
     return 0;
